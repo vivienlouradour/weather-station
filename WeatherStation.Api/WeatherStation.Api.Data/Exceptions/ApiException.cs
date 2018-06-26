3e42142f554935306df5d661c656eae34247f077
Vivien Louradour <27066260+vivienlouradour@users.noreply.github.com>
2018-06-26 22:45:18 +0200

    clean master

--- WeatherStation.Api/WeatherStation.Api.Data/Exceptions/ApiException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WeatherStation.Api.Data.Exceptions
-{
-    public class ApiException : Exception
-    {
-        protected ApiException(string message) : base(message){ }
-    }
-}
