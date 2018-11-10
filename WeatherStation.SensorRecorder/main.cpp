56b24b3c4f79b1b274c91c4e7207ea7c4a14c1f5
vivienlouradour <27066260+vivienlouradour@users.noreply.github.com>
2018-11-10 16:45:35 +0100

    add current date function

--- WeatherStation.SensorRecorder/main.cpp
+++ WeatherStation.SensorRecorder/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <stdio.h>
+#include <string>
 #include "I2cReader.h"
 
 using namespace std;
+
+/**
+ * Write current dateTime in char* parameter
+ * Formated to suit API request
+ */
+void writeFormatedCurrentDate(char* date){
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    sprintf(date, "%d-%02d-%02dT%02d:%02d:%02d.000Z", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+}
+
 int main(){
+   char currentDate[256];
     cout << "hello world! "<< endl;
-    
+    writeFormatedCurrentDate(currentDate);
+    cout << currentDate << endl;
     I2cReader i2cReader;
     try{
         float hum = i2cReader.humidity();
@@ -16,3 +32,4 @@ int main(){
         return EXIT_FAILURE;
     }
 }
+
