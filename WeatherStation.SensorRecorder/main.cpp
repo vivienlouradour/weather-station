fd8c2f7b45458c264bd11988c6b61edaa1d03333
Vivien <27066260+vivienlouradour@users.noreply.github.com>
2018-11-11 16:32:07 +0100

    add main logic

--- WeatherStation.SensorRecorder/main.cpp
+++ WeatherStation.SensorRecorder/main.cpp
@@ -7,21 +7,28 @@
 
 using namespace std;
 
-int main(){
-    Api api("host", 25, "pine64");
-    api.print();
-    stringstream stream;
+int main(int argc, char** argv){
+    //Get main args
+    if(argc < 4){
+        log("3 parameters required : host, port and broadcaster name");
+        return EXIT_FAILURE;
+    }
+    string host = string(argv[1]);
+    int port = stoi(argv[2]);
+    string broadcasterName = string(argv[3]);
 
-    char currentDate[256];
-    
-    
+    Api api(host, port, broadcasterName);
+    api.print();
+
+    stringstream stream;  
     I2cReader i2cReader;
+    char date[30];
     try{
-        //Get records
+        //Get record
         float hum = i2cReader.humidity();
         float temp = i2cReader.temperature();
         
-        //Log records
+        //Log record
         stream << "Humidity : " << hum;
         log(stream.str());
         
@@ -29,7 +36,9 @@ int main(){
         stream << "Temperature : " << temp;
         log(stream.str());
 
-
+        //Send record
+        writeFormatedCurrentDate(date);
+        api.sendRecord(temp, hum, date);
     }
     catch(const exception&){
         return EXIT_FAILURE;
